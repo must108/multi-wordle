5022557c99805c0016c673ca4876711342912510
mustaeen <136145181+must108@users.noreply.github.com>
2024-03-18 15:42:27 -0400

    add dump file

---
+++ src/data/wordledb_dump.sql
@@ -0,0 +1,16 @@
+create database wordle;
+
+create table fiveletter (
+	wordid int serial primary key,
+	words varchar(10)
+);
+
+create table fourletter (
+	wordid int serial primary key,
+	words varchar(10)
+);
+
+create table sixletter (
+	wordid int serial primary key,
+	words varchar(10)
+);
