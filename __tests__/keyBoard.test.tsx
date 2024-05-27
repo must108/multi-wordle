ff792ee29617162e4c254396e2b0e19e3153871f
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-27 13:06:27 -0400

    add keyboard tests

--- __tests__/keyBoard.test.tsx
+++ __tests__/keyBoard.test.tsx
@@ -1,7 +1,79 @@
 import { render, screen, act, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import { describe, it, expect, vi } from 'vitest';
 import Keyboard from '@/app/components/keyBoard';
+import keys from '@/app/keys';
 
 describe('Keyboard', () => {
-    
+    it('test first row buttons', () => {
+        render(<Keyboard keys={keys}/>);
+
+        for (let i = 0; i < keys[0].length; i++) {
+            const letterElement = screen.getByText(keys[0][i]);
+            const mockDispatch = vi.spyOn(window, 'dispatchEvent');
+
+            let letter = keys[0][i];
+            fireEvent.click(letterElement);
+            expect(mockDispatch).toHaveBeenCalledWith(
+                new CustomEvent('letterSent', {
+                detail: { letter }
+            }));
+        }
+    }); 
+
+    it('test second row buttons', () => {
+        render(<Keyboard keys={keys}/>);
+
+        for (let i = 0; i < keys[1].length; i++) {
+            const letterElement = screen.getByText(keys[1][i]);
+            const mockDispatch = vi.spyOn(window, 'dispatchEvent');
+
+            let letter = keys[1][i];
+            fireEvent.click(letterElement);
+            expect(mockDispatch).toHaveBeenCalledWith(
+                new CustomEvent('letterSent', {
+                detail: { letter }
+            }));
+        }
+    }); 
+
+    it('test third row buttons', () => {
+        render(<Keyboard keys={keys}/>);
+
+        for (let i = 0; i < keys[2].length; i++) {
+            const letterElement = screen.getByText(keys[2][i]);
+            const mockDispatch = vi.spyOn(window, 'dispatchEvent');
+
+            let letter = keys[2][i];
+            fireEvent.click(letterElement);
+            expect(mockDispatch).toHaveBeenCalledWith(
+                new CustomEvent('letterSent', {
+                detail: { letter }
+            }));
+        }
+    }); 
+
+    it('test enter button', () => {
+        render(<Keyboard keys={keys}/>);
+
+        const enterElement = screen.getByText(/enter/i);
+        const mockDispatch = vi.spyOn(window, 'dispatchEvent');
+
+        fireEvent.click(enterElement);
+        expect(mockDispatch).toHaveBeenCalledWith(
+            new CustomEvent('enterPressed')
+        )
+    });
+
+    it('test delete button', () => {
+        render(<Keyboard keys={keys} />);
+
+        const deleteElement = screen.getByText(/del/i);
+        const mockDispatch = vi.spyOn(window, 'dispatchEvent');
+        
+        fireEvent.click(deleteElement);
+        expect(mockDispatch).toHaveBeenCalledWith(
+            new CustomEvent('deleteLetter')
+        )
+    });
 })
