318505338b62f67bbf5f265bcf5333b25e7c5996
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-30 16:26:20 -0400

    work

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,36 +1,38 @@
-import { NextResponse } from "next/server";
-import prisma from "../lib/prisma";
+// import { NextResponse } from "next/server";
+// import prisma from "../lib/prisma";
 
-export async function getWord(word: string) {
-    let data; 
+// export async function getWord(word: string) {
+//     let data; 
 
-    switch(word) {
-        case ('fourletter'):
-            data = await prisma.fourletter.findMany();
-            break;
-        case ('fiveletter'):
-            data = await prisma.fiveletter.findMany();
-            break;
-        case ('sixletter'):
-            data = await prisma.sixletter.findMany();
-            break;
-        default:
-            data = await prisma.fiveletter.findMany();
-            break;
-    }
+//     switch(word) {
+//         case ('fourletter'):
+//             data = await prisma.fourletter.findMany();
+//             break;
+//         case ('fiveletter'):
+//             data = await prisma.fiveletter.findMany();
+//             break;
+//         case ('sixletter'):
+//             data = await prisma.sixletter.findMany();
+//             break;
+//         default:
+//             data = await prisma.fiveletter.findMany();
+//             break;
+//     }
 
-    return data.map(item => item.words).filter(
-        word => word !== null) as string[];
-}
+//     return data.map(item => item.words).filter(
+//         word => word !== null) as string[];
+// }
 
-export const GET = async(req: any) => {
-    const url = new URL(req.url);
-    const param = new URLSearchParams(url.searchParams);
-    try {
-        const words = await getWord(param.get('word')!);
-        return NextResponse.json(words, { status: 200 });
-    } catch (error) {
-        console.error('error fetching data: ', error);
-        return NextResponse.error();
-    }
-}
+// export const GET = async(req: any) => {
+//     const url = new URL(req.url);
+//     const param = new URLSearchParams(url.searchParams);
+//     try {
+//         const words = await getWord(param.get('word')!);
+//         return NextResponse.json(words, { status: 200 });
+//     } catch (error) {
+//         console.error('error fetching data: ', error);
+//         return NextResponse.error();
+//     }
+// }
+
+// commented out as a server is hosted on heroku
